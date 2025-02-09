85ff9f9ff2f23c378d2410d0198d536516ac3b9d
Tanmyname <sultanbahri2304l@gmail.com>
2025-02-09 21:12:21 +0800

    Update servo.ino

---
+++ Library/Servo/servo.ino
@@ -0,0 +1,17 @@
+#include <Servo.h> 
+Servo myservo; // inisiasi nama variabel servo
+int pos = 0 ; // deklarasi variabel intuk posisi servo 
+
+void setup(){
+  myservo.attach(6); // inisiasi pin servo
+}
+
+void loop(){
+  for ( pos = 0; pos <=180; pos +=1 ){
+     myservo.write(pos);
+     delay(20);
+  }
+  for ( pos = 180; pos >=180; pos -=1 ){
+     myservo.write(pos);
+     delay(20); 
+  }
