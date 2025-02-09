5902fcd50b6cec836b785647594ae0d4d0d319e7
Tanmyname <sultanbahri2304l@gmail.com>
2025-02-09 21:41:42 +0800

    Update RunningText.ino

--- Library/Liquidcrystal/RunningText.ino
+++ Library/Liquidcrystal/RunningText.ino
@@ -3,11 +3,21 @@
 
 const int col = 16 ; //colom
 const int row = 2;  // baris
-
+int position = 0;
 LiquidCrystal lcd(0x27,col,row);
 
 void setup(){
-  
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(col,row); //Kolom dan baris 
+  lcd.clear();
 }
-
+void loop(){
+  lcd.clear();
+  position +=1;
+  lcd.setCursor(position,1);
+  if ( position < 0 || position >= col){
+    position = 0;
+  }
+}
 
